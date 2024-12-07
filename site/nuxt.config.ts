b38c7380e02fc638b8d4cd1519a2198e7316857f
Heunsig <heun3344@gmail.com>
2024-12-07 20:30:52 +0900

    chore(site): add a favicon and title

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Beanheads Vue',
+      link: [
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ]
+    }
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
   modules: ['@nuxt/ui', '@vueuse/nuxt', 'nuxt-shiki'],
