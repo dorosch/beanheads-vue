8b672e6d7a95efe6918a5cf19a0fa9a8d6cf227f
Heunsig <heun3344@gmail.com>
2024-12-09 23:09:30 +0900

    chore(site): add gtag

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -10,12 +10,16 @@ export default defineNuxtConfig({
   },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxt/ui', '@vueuse/nuxt', 'nuxt-shiki'],
+  modules: ['@nuxt/ui', '@vueuse/nuxt', 'nuxt-shiki', 'nuxt-gtag'],
   colorMode: {
     preference: 'light',
   },
   shiki: {
     bundledThemes: ['github-light-default', 'github-dark'],
     bundledLangs: ['vue-html', 'bash'],
+  },
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    id: 'G-6K665MD6XP'
   }
 })
