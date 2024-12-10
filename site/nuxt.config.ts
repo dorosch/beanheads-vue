75469ada04637da946c55996b6d6b00cc00a5267
Heunsig <heun3344@gmail.com>
2024-12-10 21:35:06 +0900

    docs: Add meta tags

--- site/nuxt.config.ts
+++ site/nuxt.config.ts
@@ -5,6 +5,19 @@ export default defineNuxtConfig({
       title: 'Beanheads Vue',
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }
+      ],
+      meta: [
+        { name: 'description', content: 'Create billions of unique characters by mixing and matching colors, hairstyles, and clothing.' },
+        { name: 'author', content: '@heunsig' },
+        { name: 'og:title', content: 'Beanheads Vue' },
+        { name: 'og:description', content: 'Create billions of unique characters by mixing and matching colors, hairstyles, and clothing.' },
+        { name: 'og:image', content: '/thumbnail.png' },
+        { name: 'og:url', content: 'https://beanheads-vue.heunsig.com' },
+        { name: 'og:type', content: 'website' },
+        { name: 'twitter:creator', content: '@heunsig' },
+        { name: 'twitter:title', content: 'Beanheads Vue' },
+        { name: 'twitter:description', content: 'Create billions of unique characters by mixing and matching colors, hairstyles, and clothing.' },
+        { name: 'twitter:image', content: '/thumbnail.png' },
       ]
     }
   },
